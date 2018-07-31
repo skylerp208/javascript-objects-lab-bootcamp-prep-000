9ca45be57c16b310c407255cf081ff76869cc514
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-31 02:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = {}
 function updateObjectWithKeyAndValue (object, key, value) {
-   return Object.assign({}, obj, {[key]: value})
+   return Object.assign({}, object, {[key]: value})
 }
