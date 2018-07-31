65b6030496c601213ed2c1e90ed8933d5f993620
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-31 02:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({key}, object)
   delete newObj[key]
+  return newObj
 }
