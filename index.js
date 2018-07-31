cd761cd7013e5cbb3ed7ff66f69934c859f3cbba
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-31 02:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue (object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return Object.assign(object, {[key]: value})
 }
-function deleteFromObjectByKey(object, key) {
-  var newObject = delete object[key]
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
 }
